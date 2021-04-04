50b3abb367b8c156aa9de56f9779a6950b11af0d
Johanny Vargas González <jonan-vargas23@hotmail.com>
2021-04-04 13:30:44 -0500

    Se comprueba que javascript no es asincrono

--- app.js
+++ app.js
@@ -2,3 +2,20 @@ var a = 1;
 var b = "Giovanni";
 
 console.log(a);
+console.warn(a);
+console.error(a);
+console.info(a);
+
+// 7. Javascript no es asincrono y se ejecuta secuncialmente
+
+function imprimir() {
+    for (let i = 0; i < 4000; i++) {
+        console.log('Imprimio');
+    }
+}
+
+function presionoClick() {
+    console.log('Click en boton');
+}
+
+imprimir();
