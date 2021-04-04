68448369bc5a69798510a3f59c0b5c0e5f0e34bb
Johanny Vargas González <jonan-vargas23@hotmail.com>
2021-04-04 14:04:40 -0500

    Diferenciacion de objetos por valor y por referencia

--- app.js
+++ app.js
@@ -1,21 +1,66 @@
-var a = 1;
-var b = "Giovanni";
+// var a = 1;
+// var b = "Giovanni";
 
-console.log(a);
-console.warn(a);
-console.error(a);
-console.info(a);
+// console.log(a);
+// console.warn(a);
+// console.error(a);
+// console.info(a);
 
 // 7. Javascript no es asincrono y se ejecuta secuncialmente
+// function imprimir() {
+//     for (let i = 0; i < 4000; i++) {
+//         console.log('Imprimio');
+//     }
+// }
 
-function imprimir() {
-    for (let i = 0; i < 4000; i++) {
-        console.log('Imprimio');
-    }
-}
+// function presionoClick() {
+//     console.log('Click en boton');
+// }
 
-function presionoClick() {
-    console.log('Click en boton');
-}
+// imprimir();
 
-imprimir();
+// 9. Escritura dinamica - Tipos primitivos
+// var num = 10;
+// var str = "texto";
+// var bol = true;
+// var und = undefined;
+// var nul = null;
+
+// var obj = {
+//     numero: 10,
+//     texto: "Nuevo texto",
+
+//     objHijo: {
+//         numero: 10,
+//         texto: "Nuevo texto"
+//     }
+// };
+
+// console.log(obj);
+
+// 10. Por valor y por referencia
+// Por valor (los valores primitivos siempre pasan por valor y crean su propio espacio de memoria)
+var a = 10;
+var b = a;
+
+console.log("a: ", a);
+console.log("b: ", b);
+
+a = 20;
+
+console.log("a: ", a);
+console.log("b: ", b);
+
+// Por referencia (Los objetos siempre pasan por referencia, comparten el mismo espacio de memoria)
+var c = {
+    nombre: "Juana"
+};
+var d = c;
+
+console.log("c: ", c);
+console.log("d: ", d);
+
+c.nombre = "Maria";
+
+console.log("c: ", c);
+console.log("d: ", d);
